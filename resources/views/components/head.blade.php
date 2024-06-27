7d8fb53412d5c700f29b9c88182ddf573355c313
punkestu <pangestubima89@gmail.com>
2024-06-27 10:11:52 +0700

    bug fix

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -2,10 +2,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 @vite('resources/css/app.css')
 
-<link rel="stylesheet" href="/style/leaflet.css"
-    integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
-<script src="/script/leaflet.js"
-    integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+<link rel="stylesheet" href="/style/leaflet.css" />
+<script src="/script/leaflet.js"></script>
 <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap" rel="stylesheet" />
