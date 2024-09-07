3d906deb34551a0dd74cc8c893c5e19cfae8ffc7
punkestu <pangestubima89@gmail.com>
2024-09-07 11:23:11 +0700

    update welcome screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,41 +7,51 @@
 </head>
 
 <body class="font-sans tracking-wider text-lg">
-    @include('components/header')
-    <main class="min-h-screen">
-        <section class="h-screen w-full flex items-center justify-center bg-white">
-            <img src="/images/backgrounds/background2.png" alt="background landing"
-                class="blur-sm absolute z-0 opacity-50" />
-            <div class="z-10 flex flex-col items-center gap-1 text-[#015AFF]">
-                <h1 class="text-6xl 2xl:text-8xl font-bold tracking-widest">MUTANT</h1>
+    @include('components/header', ['static' => true])
+    <main class="flex flex-col gap-5 p-8">
+        <section class="flex flex-col-reverse md:flex-row items-stretch gap-8">
+            <aside class="md:w-3/5">
+                <h1 class="text-6xl 2xl:text-8xl font-bold">MUTANT</h1>
                 <p class="text-3xl 2xl:text-4xl font-medium">
                     Sistem Mutasi AirNav
                 </p>
-            </div>
-        </section>
-        <section class="flex flex-col gap-5 p-8">
-            <div class="flex flex-col gap-2">
-                <h2 class="font-bold text-xl tracking-wider">Tentang MUTANT</h2>
-                <p class="font-medium text-lg">
+                <p class="font-medium text-lg mt-2 md:text-left text-justify">
                     Mutant adalah sebuah platform berbasis web yang dirancang untuk mengelola berbagai proses terkait
                     rotasi, demosi, dan promosi karyawan di lingkungan AirNav. Aplikasi ini dapat diakses melalui
                     browser, memudahkan pengelolaan dan pemantauan dari mana saja dan kapan saja.
                     Airmutasi membantu AirNav untuk mengelola sumber daya manusia secara lebih efektif, memastikan bahwa
                     setiap proses rotasi, demosi, dan promosi dilakukan dengan cara yang adil dan efisien.
                 </p>
-            </div>
-            <div class="grid md:grid-cols-3 gap-4">
-                <a href="/rotasi/cabang"
-                    class="bg-[#FFB72D] font-semibold text-lg 2xl:text-xl flex justify-center items-center gap-3 p-4 rounded-xl 2xl:rounded-2xl">Rotasi
-                    <img src="/images/icons/rotasi.svg" alt="rotasi" /></a>
-                <a href="/"
-                    class="bg-[#FFB72D] font-semibold text-lg 2xl:text-xl flex justify-center items-center gap-3 p-4 rounded-xl 2xl:rounded-2xl opacity-65">Demosi
-                    <img src="/images/icons/demosi.svg" alt="demosi" /></a>
-                <a href="/"
-                    class="bg-[#FFB72D] font-semibold text-lg 2xl:text-xl flex justify-center items-center gap-3 p-4 rounded-xl 2xl:rounded-2xl opacity-65">Promosi
-                    <img src="/images/icons/promosi.svg" alt="promosi" /></a>
-            </div>
+                <div class="grid md:grid-cols-3 gap-4 mt-2">
+                    <div class="bg-black text-white p-4 rounded-md">
+                        <p class="font-semibold text-2xl">000+</p>
+                        <p class="opacity-60">Branch Offices</p>
+                    </div>
+                    <div class="bg-black text-white p-4 rounded-md">
+                        <p class="font-semibold text-2xl">000+</p>
+                        <p class="opacity-60">Employees</p>
+                    </div>
+                    <div class="bg-black text-white p-4 rounded-md">
+                        <p class="font-semibold text-2xl">000+</p>
+                        <p class="opacity-60">Years of Experience</p>
+                    </div>
+                </div>
+            </aside>
+            <aside class="md:w-2/5">
+                <img class="h-full object-cover rounded-md" src="/images/backgrounds/LOGIN.png" alt="">
+            </aside>
         </section>
+        <div class="grid md:grid-cols-3 gap-4">
+            <a href="/rotasi/cabang"
+                class="bg-black text-white font-semibold text-lg 2xl:text-xl flex flex-col justify-center items-center gap-3 p-8 rounded-xl 2xl:rounded-2xl"><img
+                    src="/images/icons/rotasi.svg" alt="rotasi" />Rotasi</a>
+            <a href="/"
+                class="bg-black text-white font-semibold text-lg 2xl:text-xl flex flex-col justify-center items-center gap-3 p-8 rounded-xl 2xl:rounded-2xl opacity-65">
+                <img src="/images/icons/demosi.svg" alt="demosi" />Demosi</a>
+            <a href="/"
+                class="bg-black text-white font-semibold text-lg 2xl:text-xl flex flex-col justify-center items-center gap-3 p-8 rounded-xl 2xl:rounded-2xl opacity-65">
+                <img src="/images/icons/promosi.svg" alt="promosi" />Promosi</a>
+        </div>
     </main>
     @include('components.footer')
     <script src="/script/nav.js"></script>
