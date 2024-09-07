6e1860cc055b4cbe095a4878178315c7f4ddfc75
punkestu <pangestubima89@gmail.com>
2024-09-07 11:02:01 +0700

    update header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,5 @@
 <header
-    class="bg-white font-bold flex flex-col md:flex-row items-center justify-between px-8 py-4 gap-4 {{ !empty($static) && $static ? 'sticky' : 'fixed' }} top-0 left-0 z-50 w-full shadow-black drop-shadow-lg font-sans text-lg tracking-widest font-lg">
+    class="bg-white flex flex-col md:flex-row items-center justify-between px-8 py-4 gap-4 {{ !empty($static) && $static ? 'sticky' : 'fixed' }} top-0 left-0 z-50 w-full font-sans text-lg tracking-widest font-lg">
     <a href="/"><img src="/images/logo.svg" alt="logo" /></a>
     <button class="md:hidden">☰</button>
     <nav
@@ -9,7 +9,7 @@
                     class="duration-300"></button>
             <div
                 class="max-h-0 duration-300 absolute flex flex-col gap-1 bg-white mt-4 text-gray-800 text-base font-light overflow-hidden">
-                <div class="flex flex-col gap-1 px-2 py-1 bg-[#FFB72D]">
+                <div class="flex flex-col gap-1 px-2 py-1 bg-slate-100">
                     <a href="/rotasi/cabang">Denah Rotasi</a>
                     <hr>
                     <a href="/rotasi/pengajuan">Input Personal (IP)</a>
@@ -22,30 +22,19 @@
         </div>
         <a class="text-gray-500" href="#">Demosi</a>
         <a class="text-gray-500" href="#">Promosi</a>
-        @auth
-            <a href="/akun">Akun</a>
-            <a href="/logout">Logout</a>
-        @else
-            <a href="/login">Login</a>
-        @endauth
     </nav>
-    <div class="hidden md:block absolute h-full right-0 z-40 w-40">
-        <img src="/images/backgrounds/headerAccent.svg" alt="header accent" class="h-full object-cover object-left">
+    <div>
+        @auth
+            <a href="/akun"
+                class="text-sm hover:bg-black hover:text-white duration-300 text-black border-2 border-black px-8 py-1 rounded-full">Akun</a>
+            <a href="/logout"
+                class="text-sm hover:bg-white hover:text-black duration-300 bg-black text-white border-2 border-black px-8 py-1 rounded-full">Logout</a>
+        @else
+            <a href="/login"
+                class="text-sm hover:bg-black hover:text-white duration-300 text-black border-2 border-black px-8 py-1 rounded-full">Login</a>
+        @endauth
     </div>
 </header>
-{{-- 
-<div popover id="login-popover" class="w-1/3 bg-white p-4 rounded-lg border">
-    <form action="/login" method="post" class="flex flex-col gap-2">
-        @csrf
-        <h1 class="font-bold text-xl text-center">Masuk</h1>
-        <hr>
-        <input type="email" name="email" placeholder="Email" class="px-4 py-2 border rounded-md">
-        <input type="password" name="password" placeholder="Password" class="px-4 py-2 border rounded-md">
-        <button type="submit"
-            class="font-semibold bg-[#FFB72D] hover:opacity-100 opacity-80 duration-200 px-4 py-2 rounded-md">Login</button>
-        <a href="/forget-password" class="text-blue-500 underline text-sm">Lupa password?</a>
-    </form>
-</div> --}}
 
 <script>
     const rotasiNav = document.getElementById('rotasi-nav');
