82fffadcc34ba098edde9567b5c031dffcd5abf1
punkestu <pangestubima89@gmail.com>
2024-09-07 11:30:33 +0700

    update footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,24 +1,27 @@
-<footer class="bg-[#FFB72D] flex flex-col md:flex-row items-center md:items-start justify-between gap-8 p-8 text-lg">
-    <aside class="md:max-w-[33%] flex-grow flex flex-col gap-2">
-        <h2 class="font-bold text-center">KONTAK</h2>
-        <p class="text-center md:text-left">
-            KANTOR PUSAT AIRNAV INDONESIA <br>
-            Gedung AirNav Indonesia <br>
-            Jl. Ir. H. Juanda No.1 Tangerang 15121 <br>
-            Banten - Indonesia
-        </p>
-        <ul class="flex flex-col items-center md:items-start">
-            <li>P. +62 21 5591 5000</li>
-            <li>F. +62 21 5591 5000</li>
-        </ul>
+<footer
+    class="bg-black text-white flex flex-col xl:flex-row items-center xl:items-start justify-between gap-8 p-8 text-lg">
+    <aside class="xl:max-w-[33%] flex-grow flex flex-col gap-2">
+        <h1 class="font-black text-6xl sm:text-8xl">MUTANT</h1>
     </aside>
-    <aside class="md:max-w-[33%] flex-grow md:flex-grow-0 flex flex-col items-center md:items-start gap-2">
-        <h2 class="font-bold text-center md:text-right">LAYANAN</h2>
-        <ul class="md:ms-4 flex flex-col items-center md:items-start md:[&_li]:list-disc">
+    <aside class="xl:max-w-[33%] flex-grow xl:flex-grow-0 flex flex-col items-center xl:items-start gap-4">
+        <h2 class="font-bold text-center xl:text-right">Services</h2>
+        <ul class="flex flex-col items-center xl:items-start gap-2">
             <li><a href="/rotasi/cabang">Denah Rotasi</a></li>
             <li><a href="/rotasi/pengajuan">Input Personal (IP)</a></li>
             <li><a href="/akun">Akun</a></li>
         </ul>
     </aside>
-    <img class="md:max-w-[34%] flex-grow" src="/images/logo.svg" alt="logo" />
+    <aside class="xl:max-w-[33%] flex-grow flex flex-col gap-4">
+        <h2 class="font-bold text-center xl:text-left">Contact Us</h2>
+        <ul class="flex flex-col items-center xl:items-start">
+            <li>P. +62 21 5591 5000</li>
+            <li>F. +62 21 5591 5000</li>
+        </ul>
+        <p class="text-center xl:text-left">
+            KANTOR PUSAT AIRNAV INDONESIA <br>
+            Gedung AirNav Indonesia <br>
+            Jl. Ir. H. Juanda No.1 Tangerang 15121 <br>
+            Banten - Indonesia
+        </p>
+    </aside>
 </footer>
