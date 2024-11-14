993c6574e4035ba7e30468e793c425b532a90f43
punkestu <pangestubima89@gmail.com>
2024-11-14 14:39:14 +0700

    fix notification error

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -10,10 +10,10 @@ class NotificationController extends Controller
     public function index()
     {
         if (auth()->user()->profile && auth()->user()->profile->cabang) {
-            $notifications =  auth()->user()->profile->cabang->notifications;
+            $notifications =  Notification::where('to', auth()->user()->profile->cabang->id)->orderBy('updated_at', 'desc')->get();
             Notification::where('to', auth()->user()->profile->cabang->id)->update(['is_read' => true]);
         } else if(auth()->user()->role->name == 'admin'){
-            $notifications = Notification::where('to', null)->order->get();
+            $notifications = Notification::where('to', null)->orderBy('updated_at', 'desc')->get();
             Notification::where('to', null)->update(['is_read' => true]);
         } else {
             $notifications = [];
