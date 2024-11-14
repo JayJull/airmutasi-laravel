53fe063fdd94e0c14ac3f89c9d61ee4eb56a70ca
punkestu <pangestubima89@gmail.com>
2024-11-14 14:26:51 +0700

    fix image count

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,7 +33,7 @@
                 </div>
             </aside>
             <aside class="md:w-2/5 aspect-video flex" id="carousel">
-                @for ($i = 0; $i < 2; $i++)
+                @for ($i = 0; $i < 7; $i++)
                     <img class="{{ $i != 0 ? 'w-0' : '' }} object-cover rounded-md duration-300"
                         src="/images/slider/{{ $i + 1 }}.jpeg" alt="thumbnail-{{ $i }}">
                 @endfor
