e2d176539d9733de7d742cd886f1c9fea5270061
punkestu <pangestubima89@gmail.com>
2024-11-24 07:47:06 +0700

    update chatbot

--- public/script/chatbot.js
+++ public/script/chatbot.js
@@ -1,21 +1,42 @@
-(function (w, d, s, ...args) {
-    var div = d.createElement("div");
-    div.id = "aichatbot";
-    d.body.appendChild(div);
-    w.chatbotConfig = args;
-    var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s);
-    j.defer = true;
-    j.type = "module";
-    j.src = "https://aichatbot.sendbird.com/index.js";
-    f.parentNode.insertBefore(j, f);
-})(
-    window,
-    document,
-    "script",
-    "482DAF0D-2F31-435B-B5EB-6DFF2ED5000D",
-    "onboarding_bot",
-    {
-        apiHost: "https://api-cf-ap-5.sendbird.com",
+window.__ow = window._ow || {};
+window.__ow.organizationId = "8ad0286c-d390-4516-88d4-bdbc41290243";
+window.__ow.template_id = "8a61222a-8d03-427f-8492-4b30046bf386";
+window.__ow.integration_name = "manual_settings";
+window.__ow.product_name = "chatbot";
+(function (n, t, c) {
+    function i(n) {
+        return e.h ? e._h.apply(null, n) : e._q.push(n);
     }
-);
+    var e = {
+        _q: [],
+        _h: null,
+        _v: "2.0",
+        on: function () {
+            i(["on", c.call(arguments)]);
+        },
+        once: function () {
+            i(["once", c.call(arguments)]);
+        },
+        off: function () {
+            i(["off", c.call(arguments)]);
+        },
+        get: function () {
+            if (!e._h)
+                throw new Error(
+                    "[OpenWidget] You can't use getters before load."
+                );
+            return i(["get", c.call(arguments)]);
+        },
+        call: function () {
+            i(["call", c.call(arguments)]);
+        },
+        init: function () {
+            var n = t.createElement("script");
+            (n.async = !0),
+                (n.type = "text/javascript"),
+                (n.src = "https://cdn.openwidget.com/openwidget.js"),
+                t.head.appendChild(n);
+        },
+    };
+    !n.__ow.asyncInit && e.init(), (n.OpenWidget = n.OpenWidget || e);
+})(window, document, [].slice);
