2e640f794ea17251aebe77ada6fee1f147351b54
punkestu <pangestubima89@gmail.com>
2024-11-25 10:48:06 +0700

    patch konsep item size

--- resources/views/personel/konsep.blade.php
+++ resources/views/personel/konsep.blade.php
@@ -78,17 +78,17 @@
                     class="bg-[#003285] opacity-80 hover:opacity-100 duration-200 text-white px-2 py-1 rounded-md font-medium hover:cursor-pointer text-center">Kirim</button>
             </div>
         </form>
-        <div class="flex px-4 py-2 gap-2">
+        <div class="flex flex-col px-4 py-2 gap-2">
             @foreach ($konseps as $konsep)
-                <div>
-                    <a href="{{$konsep->berkas}}" class="flex gap-2" target="_blank">{{ $konsep->name }} <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+                <div class="w-full">
+                    <a href="{{$konsep->berkas}}" class="w-full flex gap-2" target="_blank">{{ $konsep->name }} <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
                             xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
                             viewBox="0 0 24 24">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M18 14v4.833A1.166 1.166 0 0 1 16.833 20H5.167A1.167 1.167 0 0 1 4 18.833V7.167A1.166 1.166 0 0 1 5.167 6h4.618m4.447-2H20v5.768m-7.889 2.121 7.778-7.778" />
                         </svg>
                     </a>
-                    <iframe src="{{ $konsep->berkas }}" frameborder="0"></iframe>
+                    <iframe class="w-full h-[60vh]" src="{{ $konsep->berkas }}" frameborder="0"></iframe>
                 </div>
             @endforeach
         </div>
